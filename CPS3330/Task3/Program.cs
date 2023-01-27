e317b18f0eb88501f637f9b207b2ada3257ff0cb
joshua.roasa19@gmail.com <70301998+aauhsoj19@users.noreply.github.com>
2023-01-27 11:45:45 -0500

    First Commit

---
+++ CPS3330/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+//January 28, 2023
+/*
+ * Task 2 is completed by Joshua Roasa
+ */
+WriteLine("Hello, World!");
+WriteLine("Joshua Roasa");
+WriteLine("Version: {0}", Environment.Version.ToString());
+string text = System.IO.File.ReadAllText("text.txt");
+Console.WriteLine(text);
